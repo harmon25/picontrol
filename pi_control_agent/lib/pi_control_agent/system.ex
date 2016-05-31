fe923a16d8811ac1bfc90d54009359e5f99fb027
harmoN25 <nomraharmon@gmail.com>
2016-05-30 23:25:13 -0400

    more statsss

--- pi_control_agent/lib/pi_control_agent/system.ex
+++ pi_control_agent/lib/pi_control_agent/system.ex
@@ -4,10 +4,13 @@ defmodule PiCtrlAgnt.System do
   @banana_temp "/sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input"
   @pi_temp "/sys/class/thermal/thermal_zone0/temp"
 
-
-  def cpu() do
-    cpu_details = :cpu_sup.util([:per_cpu])
-    %{@node_name => %{avg1: :cpu_sup.avg1, avg5: :cpu_sup.avg5, avg15: :cpu_sup.avg15, details: cpu_details }}
+  def cpu_perf() do
+    %{@node_name => %{"avg1" => :cpu_sup.avg1,
+                      "avg5" => :cpu_sup.avg5,
+                      "avg15" => :cpu_sup.avg15,
+                      "details" => :cpu_sup.util([:per_cpu])
+                    }
+      }
   end
 
   def disks() do
@@ -35,6 +38,24 @@ defmodule PiCtrlAgnt.System do
     %{@node_name => interfaces}
   end
 
+  def kernel() do
+    result = Porcelain.shell("uname -mrs")
+    k = result.out |> String.strip
+    %{@node_name => k}
+  end
+
+  def users() do
+    result = Porcelain.shell("who")
+    user_list = result.out |> String.split("\n", trim: true) |> Enum.map(&(String.split(&1, " ", trim: true)))
+    %{@node_name => user_list}
+  end
+
+  def uptime() do
+     {:ok, file_pid} = File.open "/proc/uptime", [:read]
+      u = IO.binread(file_pid, :all) |> String.split |> List.first |> String.to_float
+     %{@node_name =>   u}
+  end
+
   def net_stats() do
     {:ok, interface_info} = :inet.getifaddrs()
      {:ok, file_pid} = File.open "/proc/net/dev", [:read]
@@ -48,14 +69,27 @@ defmodule PiCtrlAgnt.System do
       "BCM" <> _ ->  File.open @pi_temp, [:read]
       "sun" <> _ ->  File.open @banana_temp, [:read]
     end
-   temp = IO.binread(file_pid, :all)
+   temp = IO.binread(file_pid, :all) |> String.strip |> String.to_integer(10)
+   c = temp/1000
+   f = (c*1.8) + 32
+   %{@node_name => {c,f}}
   end
 
+  def distro() do
+    {:ok, file_pid} = File.open "/etc/os-release", [:read]
+     [distro] = Enum.filter IO.stream(file_pid, :line), &(String.contains?(&1, "PRETTY"))
+     distro_stripped =  String.split(distro, "=", trim: true) |> List.last |>  String.strip |> String.strip(?")
+    %{@node_name => distro_stripped}
+  end
 
-  def cpu_type() do
+  def cpu_name() do
+     %{@node_name => cpu_type}
+  end
+
+  defp cpu_type() do
      {:ok, file_pid} = File.open "/proc/cpuinfo", [:read]
       [hw] = Enum.filter IO.stream(file_pid, :line), &(String.contains?(&1, "Hardware"))
-      String.split(hw) |> List.last |> String.strip
+      String.split(hw, " ", trim: true) |> List.last |> String.strip
   end
 
   defp parse_net_stats(data) do
@@ -76,9 +110,6 @@ defmodule PiCtrlAgnt.System do
        }
      end)
   end
-
-
-
 
   # little function to pad a MAC address byte
   defp pad_mac(string_digit) when byte_size(string_digit) != 2, do: "0#{string_digit}"
